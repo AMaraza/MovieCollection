91654160624163c62506e167e4e16c6da68da171
AMaraza <allymaraza@outlook.com>
2026-01-12 16:14:42 -0500

    Added modular movie adding system for each list

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Hello World")
+const listNameInput = document.querySelector("#list-title")
+const addButton = document.querySelectorAll(".add-button")
+const movieCount = document.querySelector("#movie-count")
+let totalMovies = 0
+
+
+function AddMovie(buttonElement){
+    const addMovie = buttonElement.parentNode;
+    const newElement = document.createElement('div');
+    const newParagraph = document.createElement('input');
+    newParagraph.value = "Movie Name";
+    newParagraph.classList.add("movie-title");
+    newElement.appendChild(newParagraph);
+    newElement.classList.add("movie");
+    addMovie.before(newElement);
+    totalMovies++;
+    movieCount.textContent = `Total Movies: ${totalMovies}`; 
+}
+
+function ResetMovies(){
+    const movies = document.querySelectorAll('.movie');
+    movies.forEach(element => {
+        element.remove();
+    });
+    totalMovies = 0;
+    movieCount.textContent = `Total Movies: ${totalMovies}`;
+}
